75531bc0ff71453a34ee7a14c47fd5e9ad008668
jinwook <kju1996305@naver.com>
2018-12-07 22:33:28 +0900

    wevity

--- craw1.py
+++ craw1.py
@@ -7,18 +7,23 @@ import requests
 import re
 
 #someone use this project you shoud change DRIVER_DIR  cuz your chromedriver.exe is not same point
-driver = webdriver.Chrome()
+DRIVER_DIR = "C:/Users/user/Desktop/chromedriver_win32/chromedriver.exe"
+driver = webdriver.Chrome(DRIVER_DIR)
 driver.implicitly_wait(1)
-endcheck = True
-pagenum=18 #pagenumber
+endcheck = True#if information number is done it change False
+pagenum=1 #pagenumber
 infonum=2 #cuz number start 2 not 1
-cnt=1 #crawling number
+cnt1=0 #wevity crawling number
+cnt2=0 #thinkgood crawling number
+
 
 #다음에 pagenum <=100 말고 무한루프로 만들기
-while(pagenum<=100):
+#while(endcheck):
+while(endcheck):    
     driver.get('https://www.wevity.com/?c=find&s=1&mode=ing&gp='+str(pagenum)+'')
     time.sleep(2)
 
+    #while(endcheck):
     while(infonum<=16):
         try:
             time.sleep(1)
@@ -32,26 +37,26 @@ while(pagenum<=100):
                     
             
             #time.sleep(1)
-            print("----- "+str(pagenum)+" page, "+str(infonum-1)+" information -----")
+            print("----- receipt page "+str(pagenum)+" page, "+str(infonum-1)+" information -----")
             # (공모전이름)
             re_title = re.compile(">(.*)</h6>", re.MULTILINE)
             title_tag = re_title.findall(html)
             print("title:")
             print('\n'.join(title_tag))
 
-            #(주관)
+            #(organization)
             re_com = re.compile("주관</span>\s*(.*)\s*</li>", re.MULTILINE)
             com_tag = re_com.findall(html)
             print("com:")
             print('\n'.join(com_tag))
 
-            #(기간)
+            #(term)
             re_day = re.compile("기간</span>\s*(.*)\s*<span", re.MULTILINE)
             day_tag = re_day.findall(html)
             print("when:")
             print('\n'.join(day_tag))
 
-            #(내용)
+            #(information)
             re_info = re.compile("viewContents\">\s*(.*)</div>", re.MULTILINE)
             info_tag = re_info.findall(html)
             print("info:")
@@ -60,7 +65,7 @@ while(pagenum<=100):
             info=info.replace("/upload","https://www.wevity.com/upload")
             print(info)
 
-            #(이미지)
+            #(image)
             img_info = re.compile("<img src=\"(/upload/contest.*)\"\s*alt=\"공", re.MULTILINE)
             img_tag = img_info.findall(html)
             print("img:")
@@ -70,13 +75,15 @@ while(pagenum<=100):
             print(img)
 
 
-            #(홈페이지)
+            #(homepage)
             re_page = re.compile("홈페이지</span>\s*<a href=\"(.*?)\"", re.MULTILINE)
             page_tag = re_page.findall(html)
             print("page:")
             print('\n'.join(page_tag))
 
             cnt = cnt+1
+            
+            
             infonum=infonum+1
 
             driver.back()
@@ -93,23 +100,88 @@ while(pagenum<=100):
 #-------------------------------------------------------------------------------------
 #------------------to be receipt crawling ----------------------------------------------------
 
+endcheck = True
+
+pagenum2=1
+infonum2=2
+while(endcheck):
+    driver.get('https://www.wevity.com/?c=find&s=1&mode=future&gp='+str(pagenum2)+'')
+    time.sleep(2)
+
+    while(infonum2 <=16):
+        try:
+            time.sleep(1)
+            #driver.find_element_by_xpath('//*[@id="container"]/div[2]/div[1]/div[2]/div[3]/div/ul/li['+str(infonum)+']/div[1]/a').click()
+            driver.find_element_by_xpath('//*[@id="container"]/div[2]/div[1]/div[2]/div[3]/div/ul/li['+str(infonum2)+']/div[1]/a').click()
+            URL=(driver.current_url)
+            res=requests.get(URL)
+            html=res.text
+            
+            time.sleep(1)
+                    
+            
+            #time.sleep(1)
+            print("-----to be receipt "+str(pagenum2)+" page, "+str(infonum2-1)+" information -----")
+            # (공모전이름)
+            re_title = re.compile(">(.*)</h6>", re.MULTILINE)
+            title_tag = re_title.findall(html)
+            print("title:")
+            print('\n'.join(title_tag))
+
+            #(organization)
+            re_com = re.compile("주관</span>\s*(.*)\s*</li>", re.MULTILINE)
+            com_tag = re_com.findall(html)
+            print("com:")
+            print('\n'.join(com_tag))
+
+            #(term)
+            re_day = re.compile("기간</span>\s*(.*)\s*<span", re.MULTILINE)
+            day_tag = re_day.findall(html)
+            print("when:")
+            print('\n'.join(day_tag))
+
+            #(information)
+            re_info = re.compile("viewContents\">\s*(.*)</div>", re.MULTILINE)
+            info_tag = re_info.findall(html)
+            print("info:")
+
+            info='\n'.join(info_tag)
+            info=info.replace("/upload","https://www.wevity.com/upload")
+            print(info)
+
+            #(image)
+            img_info = re.compile("<img src=\"(/upload/contest.*)\"\s*alt=\"공", re.MULTILINE)
+            img_tag = img_info.findall(html)
+            print("img:")
+
+            img='\n'.join(img_tag)
+            img=img.replace("/upload","https://www.wevity.com/upload")
+            print(img)
 
 
+            #(homepage)
+            re_page = re.compile("홈페이지</span>\s*<a href=\"(.*?)\"", re.MULTILINE)
+            page_tag = re_page.findall(html)
+            print("page:")
+            print('\n'.join(page_tag))
+
+            cnt = cnt1+1
+            infonum2=infonum2+1
+
+            driver.back()
+            
+        except NoSuchElementException:# if pagenum == None 
+            endcheck = False
+            break
+    pagenum2=pagenum2+1        
+    infonum2=2  #to reset num because we go next page
+    
+    
+    if endcheck == False:
+        print("to be receipt crawling is finish ")
+        print("wevity server crawling num : "+str(cnt1))
+        break
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-print("total crawling num : "+str(cnt-1))
+print("total crawling num : "+str(cnt1)+str(cnt2))
 
