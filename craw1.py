61585eff91d4d4465ab7e614d108a493041611aa
jinwook <kju1996305@naver.com>
2018-12-07 17:46:07 +0900

    wevity

--- craw1.py
+++ craw1.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
 #wevity contest
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import StaleElementReferenceException
+import time
+import requests
+import re
 
 #someone use this project you shoud change DRIVER_DIR  cuz your chromedriver.exe is not same point
 DRIVER_DIR = "C:/Users/user/Desktop/chromedriver_win32/chromedriver.exe"
@@ -15,37 +15,66 @@ pagenum=1 #pagenumber
 infonum=2 #cuz number start 2 not 1
 cnt=1 #crawling number
 
-while(pagenum<2):
+while(pagenum<=16):
     driver.get('https://www.wevity.com/?c=find&s=1&mode=ing&gp='+str(pagenum)+'')
     time.sleep(2)
 
     while(infonum<=16):
         try:
-            print("----- "+str(pagenum)+" page, "+str(infonum-1)+" information -----")
             time.sleep(1)
-
             driver.find_element_by_xpath('//*[@id="container"]/div[2]/div[1]/div[2]/div[3]/div/ul/li['+str(infonum)+']/div[1]/a').click()
 
-            info = driver.find_element_by_class_name("cd-info-list")
-            detail = driver.find_element_by_class_name("comm-desc")
-            """
-            txtfile_info=open("C:/Users/user/Desktop/aven_info.txt", "a", encoding='UTF16')
-            txtfile_detail=open("C:/Users/user/Desktop/aven_detail.txt", "a", encoding='UTF16')
+            URL=(driver.current_url)
+            res=requests.get(URL)
+            html=res.text
 
-            for q1 in info:
-                infotext = q1.text
-            txtfile_info.write(str(infotext))
-            txtfile_info.write(",")
+            time.sleep(1)
 
-            for q2 in detail:
-                detailtext = q2.text
-            txtfile_detail.write(str(detailtext))
-            txtfile_detail.write(",")
-            """
-            print("                      -------------   list   ------------- \n")
-            print(info.text)
-            print("                      -------------  article ------------- \n")
-            print(detail.text)
+
+            #time.sleep(1)
+            print("----- "+str(pagenum)+" page, "+str(infonum-1)+" information -----")
+            # (공모전이름)
+            re_title = re.compile(">(.*)</h6>", re.MULTILINE)
+            title_tag = re_title.findall(html)
+            print("title:")
+            print('\n'.join(title_tag))
+
+            #(주관)
+            re_com = re.compile("주관</span>\s*(.*)\s*</li>", re.MULTILINE)
+            com_tag = re_com.findall(html)
+            print("com:")
+            print('\n'.join(com_tag))
+
+            #(기간)
+            re_day = re.compile("기간</span>\s*(.*)\s*<span", re.MULTILINE)
+            day_tag = re_day.findall(html)
+            print("when:")
+            print('\n'.join(day_tag))
+
+            #(내용)
+            re_info = re.compile("viewContents\">\s*(.*)</div>", re.MULTILINE)
+            info_tag = re_info.findall(html)
+            print("info:")
+
+            info='\n'.join(info_tag)
+            info=info.replace("/upload","https://www.wevity.com/upload")
+            print(info)
+
+            #(이미지)
+            img_info = re.compile("<img src=\"(/upload/contest.*)\"\s*alt=\"공", re.MULTILINE)
+            img_tag = img_info.findall(html)
+            print("img:")
+
+            img='\n'.join(img_tag)
+            img=img.replace("/upload","https://www.wevity.com/upload")
+            print(img)
+
+
+            #(홈페이지)
+            re_page = re.compile("홈페이지</span>\s*<a href=\"(.*?)\"", re.MULTILINE)
+            page_tag = re_page.findall(html)
+            print("page:")
+            print('\n'.join(page_tag))
 
             cnt = cnt+1
             infonum=infonum+1
@@ -54,10 +83,9 @@ while(pagenum<2):
 
         except NoSuchElementException:
             print("----------NoSuchElementException----------")
-            infonum=infonum+1
+            #infonum=infonum+1
     infonum=2  #to reset num because we go next page
     pagenum=pagenum+1
 
 print("total crawling num : "+str(cnt-1))
 
-#여기서 접수기간 2018-12-05 ~ 2018-12-30 D-24 에서 정규표현식 써서 D뒤에 -이면 ㅇㅋ 아니면 무시
