1ab913ae704a94f814c7bd33f368bc4fa20e1dc2
gps <gofeel8@hanmail.net>
2018-12-09 19:34:02 +0900

    more clean

---
+++ web/crawling_comment.php
@@ -0,0 +1,95 @@
+<?php
+	$sql = 'select * from avengers_comment2 where co_no=co_order and board=' . $bNo;
+	$result = $conn->query($sql);
+
+?>
+<div id="comments">
+
+
+		<?php
+			while($row = $result->fetch_assoc()) {
+		?>
+		<ul >
+
+					<li>
+        	<form action="./delete_update.php" method="post">
+          <input type="hidden" name="crawl_co_no" value="<?php echo $row['co_no']?>">
+          <input type="hidden" name="back" value="<?php echo $bNo?>">
+					<address>
+				</address>
+					<header>
+							<figure class="avatar"><img src="images/avatar.png" alt=""></figure>
+							<address class="">
+
+								By <a><?php echo $row['co_id']?></a>
+							</address>
+						<time datetime=<?php echo $row['writedate']?>><?php echo $row['writedate']?></time>
+
+					</header>
+
+        <div class="comcont"><?php echo $row['co_content']?></div>
+				<?php
+				  if(isset($_SESSION['username'])){
+
+					if($_SESSION['username'] == $row['co_id'] OR $_SESSION['username'] == '관리자'){
+
+				 ?>
+            <button type="submit" class="float-right btn text-white btn-danger"style="float: right;">삭제</button>
+						<?php
+						}
+					}
+						 ?>
+        </form>
+
+			</li>
+		</ul>
+		<?php } ?>
+
+</div>
+
+
+
+
+
+<?php
+  if(isset($_SESSION['username'])){
+ ?>
+ <div class="writecomment">
+<h2>댓글 쓰기</h2>
+<form action="crawling_comment_update.php" method="post">
+  	<input type="hidden" name="board" value="<?php echo $bNo?>">
+	<table>
+		<tbody>
+			<tr>
+        <th ><label for="bID">작성자: </label></th>
+        <td >
+          <?php
+          echo "<h4>".$_SESSION['username']."</h4>";
+
+                          ?>
+          <input type="hidden" name="coId" value="<?php echo $_SESSION['username']; ?>">
+        </tr>
+
+				           <div class="block clear">
+				<tr>
+
+					<th ><label for="coContent">내용:</label></th>
+					<td><input type="text" name="coContent" id="name" value="" size="80" required></td>
+
+					<!-- <td><textarea name="coContent" id="coContent"></textarea></td> -->
+
+				</tr>
+		</tbody>
+	</table>
+	<div class="btnSet">
+		<input type="submit" class="btnSubmit btn" value="댓글작성">
+	</div>
+</form>
+
+</div>
+<?php
+}
+else {
+	echo "로그인해야 댓글을 작성할 수 있습니다.";
+}
+ ?>
---
+++ web/view.php
@@ -0,0 +1,122 @@
+<?php
+session_start();
+require_once("./db_info.php");
+  $bNo = $_GET['bno'];
+	if(!empty($bNo)) {
+		$sql = 'UPDATE avengers_board set see = see + 1 where id = ' . $bNo;
+    $result=mysqli_query($conn,$sql);
+		if(empty($result)) {
+			?>
+			<script>
+				alert('오류가 발생했습니다.');
+				history.back();
+			</script>
+			<?php
+		}
+	}
+  $sql ='SELECT title, comment, writedate, see, user from avengers_board where id = ' . $bNo;
+  $result=mysqli_query($conn,$sql);
+  $row=mysqli_fetch_array($result);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>공지사항</title>
+<link rel="stylesheet" href="./css/style.css">
+<link href="css/layout.css" rel="stylesheet" type="text/css" media="all">
+<link href="css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="./css/board.css" />
+
+</head>
+
+<body>
+  <div id="grid2">
+<a href="index.php">
+<img src="images/logo.png"  height="200px" width="500" class="center">
+</a>
+  </div>
+
+<div id="grid">
+  <div id="login">
+    <ul class="list-group">
+      <li class="list-group-item"><a href="index.php"> 공모전</a></li>
+	    <li class="list-group-item"><a href="board.php"> 자유게시판</a></li>
+    </ul>
+  <div id="jb-sidebar-right">
+    <?php
+    if(!isset($_SESSION['username']))
+    {
+
+     echo '<form  method="post" action="login.php" > <!--post로 login.php로 로그인정보 보냄-->
+                 <div class="form-group">
+               <label for="ID">ID : </label> <br/>
+               <input type="text" class="form-control" id="ID" name="id" placeholder="ID를입력해주세요" /><br />
+
+               </div>
+               <label for="PASSWORD"> PASSWORD :</label> <br/>
+               <input type="password"  class="form-control"  class="form-control" name="password" placeholder="비밀번호를입력해주세요"  /> <br />
+               <input type="submit" value="로그인"  class="btn btn-info"/>
+               <input type="button" name ="버튼" value="회원가입" class="btn btn-info" onclick="location.href=\'join.php\'";>
+              </form>';
+
+    }
+    else
+    {
+      echo "<h3>".$_SESSION['username'].'님</h3><br>';
+      echo '<input type="button" name ="button" value="로그아웃" class="btn btn-info" onclick="window.location.href=\'logout.php\'">';           //로그아웃 페이지 링크.
+
+    }
+
+        ?>
+  </div>
+</div>
+  <div id="article">
+
+<article class="boardArticle">
+<h3>공지사항</h3>
+<div id="boardView">
+<h3 id="boardTitle"><?php echo $row['title']?></h3>
+<div id="boardInfo">
+<span id="boardID">작성자: <?php echo $row['user']?></span>
+<span id="boardDate">작성일: <?php echo $row['writedate']?></span>
+<span id="boardHit">조회: <?php echo $row['see']?></span>
+</div>
+<div id="boardContent"><?php echo $row['comment']?></div>
+<?php
+  if(isset($_SESSION['username'])){
+
+  if($_SESSION['username'] == $row['user'] OR $_SESSION['username'] == '관리자'){
+
+ ?>
+<div  style="float: right;">
+  <form action="./write.php" method="get"class="inlineform">
+    <input type="hidden" name="bno" value="<?php echo $bNo;?>">
+    <button type="submit" class="btnSubmit btn">수정</button>
+  </form>
+  &nbsp;
+<form action="./delete_update.php" method="post" class="inlineform">
+  <input type="hidden" name="bno" value="<?php echo $bNo;?>">
+  <button type="submit" class="float-right btn text-white btn-danger">삭제</button>
+</form>
+</div>
+
+<?php
+}
+}
+ ?>
+<div id="boardComment">
+  <?php require_once('./comment.php')?>
+</div>
+</div>
+</div>
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script src="js/bootstrap.js"></script>
+</article>
+
+
+</body>
+</html>
